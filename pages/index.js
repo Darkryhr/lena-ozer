5163ea60733daa2954c0971eeec22567a0311661
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-29 21:20:38 +0300

    hero font resize

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { GiAbstract076 } from 'react-icons/gi';
 const Home = () => {
   return (
     <div className='mx-auto h-screen flex justify-center flex-col px-8 relative max-w-7xl '>
-      <h1 className='md:text-8xl text-5xl font-extrabold font-serif z-10'>
+      <h1 className='md:text-8xl text-6xl font-extrabold font-serif z-10'>
         Hello, my name is Lena.
       </h1>
       <p className='mt-2 text-lg text-gray-800 font-medium z-10'>
