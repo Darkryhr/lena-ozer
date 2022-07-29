908aeec4fe8b1f9ad9949e98410fd1a144cf86f8
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-29 21:36:35 +0300

    handle outside click

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { BiMenuAltRight } from 'react-icons/bi';
@@ -25,12 +25,23 @@ const routes = [
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  const handleClickOutside = event => {
+    if (ref.current && !ref.current.contains(event.target)) setOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () =>
+      document.removeEventListener('click', handleClickOutside, true);
+  }, []);
 
   return (
     <nav className='flex justify-between px-6 py-4 items-center max-w-7xl mx-auto w-full  z-50'>
       <div>
         <Link href='/' passHref>
-          <div className='flex items-center'>
+          <div className='flex items-center' onClick={() => setOpen(false)}>
             <TbGlobe
               size={20}
               style={{
@@ -44,6 +55,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div
+        ref={ref}
         className={`md:block md:relative md:top-0 md:w-fit md:p-0 ${
           open
             ? 'flex flex-col absolute top-[64px] left-0 space-y-8 w-full pt-4 pb-6 bg-rose-100 md:bg-transparent'
@@ -51,7 +63,7 @@ const Navbar = () => {
         }`}
       >
         {routes.map(route => (
-          <CustomLink href={route.href} key={route.href}>
+          <CustomLink href={route.href} key={route.href} setOpen={setOpen}>
             {route.name}
           </CustomLink>
         ))}
@@ -71,13 +83,14 @@ const ActiveBorder = () => (
   <div className='h-1 w-8 bg-rose-400 absolute -bottom-6 left-1/2 transform -translate-x-1/2 hidden md:block'></div>
 );
 
-const CustomLink = ({ children, href }) => {
+const CustomLink = ({ children, href, setOpen }) => {
   const router = useRouter();
 
   return (
     <Link href={href} passHref>
       <a
         className={`md:ml-5 ml-8 py-2 md:p-0 text-sm font-semibold hover:underline  underline-offset-1`}
+        onClick={() => setOpen(false)}
       >
         <span className='relative'>
           {children}
