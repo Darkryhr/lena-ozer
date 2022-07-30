19863776c6485a240b180bb0accabf892a690125
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-30 10:20:14 +0300

    about page fixes

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <>
+      {/* TODO: add color to header only after some scrolling */}
       <header className='w-full bg-rose-50 shadow-sm fixed top-0 left-1/2 transform -translate-x-1/2 z-50'>
         <Navbar />
       </header>
--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,13 @@ import React from 'react';
 const About = () => {
   return (
     <div className='mx-auto h-screen flex items-center px-8 max-w-7xl justify-center'>
-      <div className='flex md:items-start justify-center space-x-6 flex-shrink-0 md:flex-row flex-col items-center'>
-        <div className='h-56 w-56 bg-gray-300 rounded-full'></div>
-        <div className='flex flex-col w-1/2 mt-6'>
-          <h1 className='text-4xl font-extrabold font-serif'>Yours truly,</h1>
-          <p className='text mt-3 text-gray-600 font-medium  max-w-3xl'>
+      <div className='flex items-start justify-center md:space-x-6 md:flex-row flex-col'>
+        <div className='min-w-[200px] min-h-[200px] bg-gray-300 rounded-full'></div>
+        <div className='flex flex-col mt-6 md:max-w-2xl text-clip'>
+          <h1 className='text-5xl font-extrabold font-serif text-gray-900'>
+            Yours truly,
+          </h1>
+          <p className='mt-3 font-body text-gray-800'>
             Id culpa commodo voluptate pariatur sit mollit consectetur laboris
             Lorem tempor dolor ut occaecat. Nisi esse labore laborum Lorem anim
             velit ipsum irure sint cillum in. Aliqua amet culpa consectetur
