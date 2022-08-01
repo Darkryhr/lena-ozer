b76b65ad3a7866662ecb00f35c24ea14053149f8
Gabriel Lellouche <gabyk800@gmail.com>
2022-08-01 10:18:51 +0300

    fixed section wrapper z-index

--- components/SectionWrapper.jsx
+++ components/SectionWrapper.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const SectionWrapper = ({ children, delay = 0 }) => {
+const SectionWrapper = ({ children, delay = 0, styles = {} }) => {
   return (
     <motion.section
       initial={{ y: 10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, delay }}
+      style={styles}
     >
       {children}
     </motion.section>
