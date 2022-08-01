e708fc0e544582664d05ce8554e86cb5792c2b7e
Gabriel Lellouche <gabyk800@gmail.com>
2022-08-01 14:22:44 +0300

    updated loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 const Loader = () => {
   return (
     <div className='flex items-center justify-center w-full h-screen absolute bg-gradient-to-r from-rose-100 to-teal-100 z-50'>
-      <div className='w-40 h-40 border-t-4 border-b-4 border-rose-400 rounded-full animate-spin'></div>
+      <div className='flex flex-col items-center justify-center'>
+        <AiOutlineLoading3Quarters
+          className='animate-spin fill-rose-400'
+          size={30}
+        />
+        <p className='text-sm font-semibold mt-3 text-rose-500'>Loading...</p>
+      </div>
     </div>
   );
 };
